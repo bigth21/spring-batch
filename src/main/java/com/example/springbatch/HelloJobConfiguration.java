bd8d8f45237afb47fb397aa26731153535c8b18f
bigth <c.bigth@gmail.com>
2022-07-12 22:31:26 +0900

    Update DB

--- src/main/java/com/example/springbatch/HelloJobConfiguration.java
+++ src/main/java/com/example/springbatch/HelloJobConfiguration.java
@@ -2,6 +2,7 @@ package com.example.springbatch;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersIncrementer;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
