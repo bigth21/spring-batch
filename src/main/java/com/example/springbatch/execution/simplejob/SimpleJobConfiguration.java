e7d9d54be54ac84753d4f94eb32085625d8c968a
bigth <c.bigth@gmail.com>
2022-08-11 02:35:51 +0900

    Update concept of simplejob

---
+++ src/main/java/com/example/springbatch/execution/simplejob/SimpleJobConfiguration.java
@@ -0,0 +1,53 @@
+package com.example.springbatch.execution.simplejob;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SimpleJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob() {
+        return jobBuilderFactory.get("simpleJob.job")
+                .start(step1())
+                .next(step2())
+                .next(step3())
+                .incrementer(new RunIdIncrementer())
+                .validator(parameters -> {})
+                .build();
+    }
+
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
+                .build();
+    }
+
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
+                .build();
+    }
+
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((contribution, chunkContext) -> {
+                    chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
+                    contribution.setExitStatus(ExitStatus.STOPPED);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+
+}
