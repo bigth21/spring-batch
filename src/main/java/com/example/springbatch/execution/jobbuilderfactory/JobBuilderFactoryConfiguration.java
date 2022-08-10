8faf45ae17bbd654443d6d025d43dc92dc4ac306
bigth <c.bigth@gmail.com>
2022-08-11 01:48:51 +0900

    Update SimpleJob & FlowJob related content

---
+++ src/main/java/com/example/springbatch/execution/jobbuilderfactory/JobBuilderFactoryConfiguration.java
@@ -0,0 +1,74 @@
+package com.example.springbatch.execution.jobbuilderfactory;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class JobBuilderFactoryConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob1() {
+        return this.jobBuilderFactory.get("jobBuilderFactory.simpleJob")
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .next(step2())
+                .build();
+    }
+    @Bean
+    public Job batchJob() {
+        return this.jobBuilderFactory.get("jobBuilderFactory.flowJob")
+                .incrementer(new RunIdIncrementer())
+                .start(flow())
+                .next(step5())
+                .end()
+                .build();
+    }
+
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED).build();
+    }
+
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED).build();
+    }
+
+    public Flow flow() {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+        flowBuilder.start(step3())
+                .next(step4())
+                .end();
+
+        return flowBuilder.build();
+    }
+
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED).build();
+    }
+
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED).build();
+    }
+
+    public Step step5() {
+        return stepBuilderFactory.get("step5")
+                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED).build();
+    }
+}
