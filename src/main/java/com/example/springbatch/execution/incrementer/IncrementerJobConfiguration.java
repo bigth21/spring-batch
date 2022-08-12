3881115890c9fe1e3f69c210cce46d6af749153a
Taekhyeon Nam <taekhyeon@Taekhyeons-MacBook-Air.local>
2022-08-12 18:39:21 +0900

    Update incrementer interface implementation

--- src/main/java/com/example/springbatch/execution/incrementer/IncrementerJobConfiguration.java
+++ src/main/java/com/example/springbatch/execution/incrementer/IncrementerJobConfiguration.java
@@ -1,32 +1,27 @@
-package com.example.springbatch.execution.validator;
+package com.example.springbatch.execution.incrementer;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 @RequiredArgsConstructor
-public class ValidatorConfiguration {
+public class IncrementerJobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
     public Job batchJob() {
-        return this.jobBuilderFactory.get("validator.job")
-                .validator(new DefaultJobParametersValidator(new String[]{"name", "date"}, new String[]{"count"}))
-//                .validator(new CustomJobParametersValidator())
+        return jobBuilderFactory.get("incrementer.job")
+                .incrementer(new CustomJobParameterIncrementer())
                 .start(step1())
                 .next(step2())
-                .next(step3())
                 .build();
     }
 
@@ -39,16 +34,6 @@ public class ValidatorConfiguration {
     public Step step2() {
         return stepBuilderFactory.get("step2")
                 .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
-                .build();
-    }
-
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .tasklet((contribution, chunkContext) -> {
-                    chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
-                    contribution.setExitStatus(ExitStatus.STOPPED);
-                    return RepeatStatus.FINISHED;
-                })
                 .build();
     }
 }
