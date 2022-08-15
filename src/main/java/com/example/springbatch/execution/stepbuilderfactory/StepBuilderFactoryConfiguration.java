d3552c48fec97bef799a36f4bbf46f0816acb8f7
Taekhyeon <c.bigth@gmail.com>
2022-08-15 16:35:29 +0900

    Compose tesklet step related configuration

--- src/main/java/com/example/springbatch/execution/stepbuilderfactory/StepBuilderFactoryConfiguration.java
+++ src/main/java/com/example/springbatch/execution/stepbuilderfactory/StepBuilderFactoryConfiguration.java
@@ -3,22 +3,17 @@ package com.example.springbatch.execution.stepbuilderfactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.item.*;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
-@Configuration
+//@Configuration
 @RequiredArgsConstructor
 public class StepBuilderFactoryConfiguration {
 
---
+++ src/main/java/com/example/springbatch/execution/taskletstep/TaskletStepConfiguration.java
@@ -0,0 +1,59 @@
+package com.example.springbatch.execution.taskletstep;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.CompletionPolicy;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class TaskletStepConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob() {
+        return jobBuilderFactory.get("taskletStep.job")
+                .start(taskBasedStep())
+                .build();
+    }
+
+    @Bean
+    public Step taskBasedStep() {
+        return stepBuilderFactory.get("taskBasedStep")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("Step1 has been executed");
+                        return RepeatStatus.FINISHED; // Same with null value
+                    }
+                })
+                .allowStartIfComplete(true) // Irrespective of success or failure, restart will always be executed;
+                .build();
+    }
+
+    @Bean
+    public Step chunkBasedStep() {
+        return stepBuilderFactory.get("chunkBasedStep")
+                .<String, String>chunk(10)
+                .reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3", "item4", "item5")))
+                .processor((ItemProcessor<String, String>) item -> item.toUpperCase())
+                .writer(items -> items.forEach(x -> System.out.println(x)))
+                .build();
+    }
+}
