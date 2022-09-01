e2e3633cdb736ad4a890c81b07b58e2019ed2093
Taekhyeon <c.bigth@gmail.com>
2022-09-02 00:20:43 +0900

    Update JobExecutionDecider-related code

---
+++ src/main/java/com/example/springbatch/flow/transition/JobExecutionDeciderConfiguration.java
@@ -0,0 +1,76 @@
+package com.example.springbatch.flow.transition;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.flow.FlowExecutionStatus;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class JobExecutionDeciderConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob() {
+        return jobBuilderFactory.get("jobExecutionDecider.job")
+                .incrementer(new RunIdIncrementer())
+                .start(step())
+                .next(decider())
+                .from(decider()).on("ODD").to(oddStep())
+                .from(decider()).on("EVEN").to(evenStep())
+                .end()
+                .build();
+    }
+
+    @Bean
+    public JobExecutionDecider decider() {
+        return new CustomDecider();
+    }
+
+    static class CustomDecider implements JobExecutionDecider {
+        @Override
+        public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
+            var runId = jobExecution.getJobParameters().getLong("run.id");
+            if (runId % 2 == 0) return new FlowExecutionStatus("EVEN");
+            else return new FlowExecutionStatus("ODD");
+        }
+    }
+
+    @Bean
+    public Step step() {
+        return stepBuilderFactory.get("startStep")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("This is the start tasklet");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Step evenStep() {
+        return stepBuilderFactory.get("evenStep")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("EvenStep has been executed");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Step oddStep() {
+        return stepBuilderFactory.get("oddStep")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("OddStep has been executed");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+}
