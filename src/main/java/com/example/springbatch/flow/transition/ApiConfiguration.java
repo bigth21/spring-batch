35a5b9fe8f137a5e3241902af3e2c3be6b8f65d2
Taekhyeon <c.bigth@gmail.com>
2022-09-01 23:40:35 +0900

    Update custom exit status related code

--- src/main/java/com/example/springbatch/flow/transition/ApiConfiguration.java
+++ src/main/java/com/example/springbatch/flow/transition/ApiConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 @RequiredArgsConstructor
 public class ApiConfiguration {
 
---
+++ src/main/java/com/example/springbatch/flow/transition/CustomExitStatusConfiguration.java
@@ -0,0 +1,64 @@
+package com.example.springbatch.flow.transition;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class CustomExitStatusConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob() {
+        return jobBuilderFactory.get("customExitStatus.job")
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .on(ExitStatus.FAILED.getExitCode()).to(step2())
+                .on("PASS").stop()
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> {
+                    contribution.setExitStatus(ExitStatus.FAILED);
+                    return null;
+                }).build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> null)
+                .listener(new PassCheckingListener()).build();
+    }
+
+    static class PassCheckingListener implements StepExecutionListener {
+
+        @Override
+        public void beforeStep(StepExecution stepExecution) {
+
+        }
+
+        @Override
+        public ExitStatus afterStep(StepExecution stepExecution) {
+            var exitCode = stepExecution.getExitStatus();
+            if (!exitCode.equals(ExitStatus.FAILED)) {
+                return new ExitStatus("PASS");
+            }
+            return null;
+        }
+    }
+}
