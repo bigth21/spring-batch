6720b17c88f18a366504c737915c108ec0cf6eab
bigth21 <taekhyeon.nam@gmail.com>
2024-11-18 22:15:52 +0900

    Add hello world job

--- src/main/java/com/example/springbatch/SpringBatchApplication.java
+++ src/main/java/com/example/springbatch/SpringBatchApplication.java
@@ -1,13 +1,42 @@
 package com.example.springbatch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @SpringBootApplication
 public class SpringBatchApplication {
+    @Autowired
+    JobRepository jobRepository;
+    @Autowired
+    PlatformTransactionManager transactionManager;
+
+    @Bean
+    Step step() {
+        return new StepBuilder("step1", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("Hello, World!");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    Job job() {
+        return new JobBuilder("job", jobRepository)
+                .start(step())
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBatchApplication.class, args);
     }
-
 }
